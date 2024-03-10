ce84a65828e0f138e6ad18479aa4b9d670c9a865
JackSkelt <davedossantos978@gmail.com>
2024-03-10 14:02:45 -0300

    refactor: create function for abstract request

--- src/client.rs
+++ src/client.rs
@@ -9,7 +9,7 @@ use serde::de::DeserializeOwned;
 use crate::{
     config::Config,
     user::{Locale, LocaleResponse, User, UserResponse},
-    util,
+    util::{self, make_request},
 };
 use tracing::{debug, trace};
 
@@ -32,84 +32,14 @@ impl Discloud {
     }
 
     pub async fn get_user_info(&self) -> Result<User, Error> {
-        let url = util::default_url("user");
-
-        debug!("Creating request client");
-        let client = Client::builder()
-            .user_agent(&self.config.user_agent)
-            .build()?;
-
-        debug!(url = url, "Making request");
-        let response = client
-            .get(url)
-            .header("api-token", &self.config.token)
-            .send()
-            .await?;
-
-        let response_status = response.status();
-
-        if !response_status.is_success() {
-            debug!(
-                status = response_status.as_u16(),
-                response_body = format!("{:?}", response.text().await),
-                "Request failed"
-            );
-            if response_status.is_server_error() {
-                return Err(Error::ServerError);
-            }
-
-            return Err(match response_status.as_u16() {
-                401 => Error::InvalidToken,
-                429 => Error::Ratelimited,
-                404 => Error::NotFound,
-                _ => Error::Unknown,
-            });
-        }
-
-        let body = response.json::<UserResponse>().await?;
-        debug!(response_body = format!("{body:?}"), "Request succeed");
+        let body: UserResponse = make_request(&self.config, Method::GET, "user").await?;
 
         Ok(body.user)
     }
 
     pub async fn set_locale(&self, locale: Locale) -> Result<(), Error> {
-        let url = util::default_url(&format!("locale/{locale}"));
-
-        debug!("Creating request client");
-        let client = Client::builder()
-            .user_agent(&self.config.user_agent)
-            .build()?;
-
-        debug!(url = url, "Making request");
-        let response = client
-            .put(url)
-            .header("api-token", &self.config.token)
-            .send()
-            .await?;
-
-        let response_status = response.status();
-
-        if !response_status.is_success() {
-            debug!(
-                status = response_status.as_u16(),
-                response_body = format!("{:?}", response.text().await),
-                "Request failed"
-            );
-            if response_status.is_server_error() {
-                return Err(Error::ServerError);
-            }
-
-            return Err(match response_status.as_u16() {
-                401 => Error::InvalidToken,
-                429 => Error::Ratelimited,
-                404 => Error::NotFound,
-                _ => Error::Unknown,
-            });
-        }
-
-        let body = response.json::<LocaleResponse>().await?;
-
-        debug!(response_body = format!("{body:?}"), "Request succeed");
+        let _: LocaleResponse =
+            make_request(&self.config, Method::PUT, &format!("locale/{locale}")).await?;
 
         Ok(())
     }
--- src/util.rs
+++ src/util.rs
@@ -1,3 +1,53 @@
+use std::fmt::Debug;
+
+use reqwest::{Client, Method};
+use serde::de::DeserializeOwned;
+use tracing::debug;
+
+use crate::{config::Config, Error};
+
 pub fn default_url(path: &str) -> String {
     format!("https://api.discloud.app/v2/{path}")
+}
+
+pub async fn make_request<T: DeserializeOwned + Debug>(
+    config: &Config,
+    method: Method,
+    path: &str,
+) -> Result<T, Error> {
+    let url = default_url(path);
+
+    debug!("Creating request client");
+    let client = Client::builder().user_agent(&config.user_agent).build()?;
+
+    debug!(url = url, "Making request");
+    let response = client
+        .request(method, url)
+        .header("api-token", &config.token)
+        .send()
+        .await?;
+
+    let response_status = response.status();
+
+    if !response_status.is_success() {
+        debug!(
+            status = response_status.as_u16(),
+            response_body = format!("{:?}", response.text().await),
+            "Request failed"
+        );
+        if response_status.is_server_error() {
+            return Err(Error::ServerError);
+        }
+
+        return Err(match response_status.as_u16() {
+            401 => Error::InvalidToken,
+            429 => Error::Ratelimited,
+            404 => Error::NotFound,
+            _ => Error::Unknown,
+        });
+    }
+
+    let body = response.json::<T>().await?;
+    debug!(response_body = format!("{body:?}"), "Request succeed");
+    Ok(body)
 }
